9aa4ec19a77f3f9bf0e625de790b4a873bffc907
tremanus <lbiyabrook@gmail.com>
2024-08-24 22:08:19 -0400

    favicon fix maybe

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <AuthProvider>
           <Header />
