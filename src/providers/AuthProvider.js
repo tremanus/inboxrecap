c2cf970b14df3d6215894c107b6befe9d3f16674
tremanus <lbiyabrook@gmail.com>
2024-08-29 22:35:31 -0400

    maybe fix

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -1,17 +1,19 @@
-// src/providers/AuthProvider.js
-"use client"; // Ensure this component is client-side
+"use client";
 
 import React from 'react';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const AuthProvider = ({ children }) => {
+  const isProduction = process.env.NODE_ENV === 'production';
+  const redirectUri = isProduction 
+    ? "https:www.inboxrecap.com" 
+    : "http://localhost:3000";
+
   return (
     <Auth0Provider
       domain="dev-ftmr2hfwyqzsehxk.us.auth0.com"
       clientId="rXWK9L5O3pNeFYogyOdsPvOFrn2x6jkI"
-      authorizationParams={{
-        redirect_uri: typeof window !== 'undefined' ? window.location.origin : ''
-      }}
+      authorizationParams={{ redirect_uri: redirectUri }}
     >
       {children}
     </Auth0Provider>
